6b59bf2850f028b2138cfcf6cdd89daaff4f4955
ayokunle <ayokunle.fatokimi@decagon.dev>
2020-12-09 15:20:30 +0100

    added a solved exercise 2 function

--- exercise2.playground/Contents.swift
+++ exercise2.playground/Contents.swift
@@ -12,3 +12,10 @@ import UIKit
  Don't forget to return the result.
  See Resources tab for more info.
  */
+func concat(_ arr1: [Int], _ arr2: [Int]) -> [Int] {
+    return arr1 + arr2
+}
+
+concat([1, 3, 5], [2, 6, 8])
+concat([7, 8], [10, 9, 1, 1, 2])
+concat([4, 5, 1], [3, 3, 3, 3, 3])
