9aa54578401d56743d3816292eecb67acd3f1d77
ayokunle <ayokunle.fatokimi@decagon.dev>
2020-12-09 14:23:16 +0100

    added the instruction and my solved function

--- exercise1.playground/Contents.swift
+++ exercise1.playground/Contents.swift
@@ -1,3 +1,25 @@
 import UIKit
 
-var str = "Hello, playground"
+/*
+ Create a function that takes two numbers as arguments (num, length) and returns an
+ array of multiples of num until the array length reaches length.
+
+ Examples
+ arrayOfMultiples(7, 5) ➞ [7, 14, 21, 28, 35]
+ arrayOfMultiples(12, 10) ➞ [12, 24, 36, 48, 60, 72, 84, 96, 108, 120]
+ arrayOfMultiples(17, 6) ➞ [17, 34, 51, 68, 85, 102]
+ 
+ Notes
+ Notice that num is also included in the returned array.
+ */
+
+func arrayOfMultiples(_ num: Int, _ length: Int) -> [Int] {
+    var outArray = [Int]()
+    for i in 1...length{
+        outArray.append(num * i)
+    }
+    return outArray
+}
+arrayOfMultiples(7, 5)
+arrayOfMultiples(12, 10)
+arrayOfMultiples(17, 6) 
