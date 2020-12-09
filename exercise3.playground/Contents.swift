a31803a0def71ce3a54f8d73db3488c4456f03c7
ayokunle <ayokunle.fatokimi@decagon.dev>
2020-12-09 15:30:58 +0100

    added exercise 3 file and instruction

---
+++ exercise3.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+/*
+ Create a function, that will for a given a, b, c, do the following:
+
+ Add a to itself b times.
+ Check if the result is divisible by c.
+ Examples
+ abcMath(42, 5, 10) ➞ false
+ // 42+42 = 84, 84+84 = 168, 168+168 = 336, 336+336 = 672, 672+672 = 1344
+ // 1344 is not divisible by 10
+
+ abcMath(5, 2, 1) ➞ true
+ abcMath(1, 2, 3) ➞ false
+ */
